3d944ba965b2a38d2be0ca53e434cbf85780b2ed
KsaweryKapela <ksawerykapela97@gmail.com>
2022-12-11 17:49:00 +0100

    Cleaned up code, fixed some errors.

--- analysis/opening_data.py
+++ analysis/opening_data.py
@@ -1,38 +1,41 @@
 import pandas as pd
 import numpy as np
 from sklearn import preprocessing
+from autocorrect import Speller
 
-def open_data():
-    file_name = 'GDT_NLP_v1_supervision.xlsx'
+def open_data(file_name, labels):
     df = pd.read_excel(io=file_name)
     df = df[~df['age'].isna()]
 
-    df['sex'] = [(male, female) for male, female in zip(df['is_male'], df['is_female'])]
-    df['nlp_2_4_5'] = [f'{nlp_2}. {nlp_4} .{nlp_5}' for nlp_2, nlp_4, nlp_5 in zip(df['nlp_2'], df['nlp_4'], df['nlp_5'])]
-    
-    age_arr = df['age'].values
-    age_norm = preprocessing.normalize([age_arr])
-    df['age_norm'] = age_norm[0]
+    print(len(df))
 
-    hours_arr = df['hours'].values
-    hours_norm = preprocessing.normalize([hours_arr])
-    df['hours_norm'] = hours_norm[0]
-
-    return df
-
-
-def choose_data(x, y, labels=[0, 1]):
-    df = open_data()
-
-    for item in x:
-        df = df[~df[item].isna()]
-
-        if labels:
-            if 'nlp' in item:
+    if labels and labels != []:
+        for item in df:
+            if 'nlp' in item and 'label' not in item:
                 for label in labels:
                     df = df[df[f'{item}_label'] != label]
 
+    print(len(df))
+    
+    df['sex'] = [(male, female) for male, female in zip(df['is_male'], df['is_female'])]
+    df['nlp_2_4_5'] = [f'{nlp_2}. {nlp_4} .{nlp_5}' for nlp_2, nlp_4, nlp_5 in zip(df['nlp_2'], df['nlp_4'], df['nlp_5'])]
+    
+    # age_arr = df['age'].values
+    # age_norm = preprocessing.normalize([age_arr])
+    # df['age_norm'] = age_norm[0]
+
+    # hours_arr = df['hours'].values
+    # hours_norm = preprocessing.normalize([hours_arr])
+    # df['hours_norm'] = hours_norm[0]
+
+    return df
+
+def choose_data(x, y, labels, file_name):
+    df = open_data(file_name, labels)
     df = df.filter([*x, y])
+    for item in x:
+        df = df[~df[item].isna()]
+    print(len(df))
     return df
 
 def get_X_Y(x_data, y_data, transformer_, df_):
@@ -43,7 +46,7 @@ def get_X_Y(x_data, y_data, transformer_, df_):
         print(item)
         if 'nlp' in item:
 
-            item_processed = transformer_.transform(df_[item])
+            item_processed = transformer_.transform(list(df_[item]))
             all_x_processed.append(item_processed)
 
         elif item == 'sex':
@@ -58,6 +61,3 @@ def get_X_Y(x_data, y_data, transformer_, df_):
     y_processed = df_[y_data]
 
     return x_processed, y_processed
-
-
-open_data()
